f177a98760c6b7b90420d8570bd97a0a3325d853
Thales Domingues <55956799+thdomingues-dev@users.noreply.github.com>
2022-04-21 16:20:06 -0300

    feat: add list-items to films-page (#1)

--- src/pages/films/[slug].tsx
+++ src/pages/films/[slug].tsx
@@ -5,11 +5,59 @@ import { GetStaticProps, GetStaticPaths } from 'next'
 // Api
 import api from '../../services/api'
 
-const Films = (props: any): ReactElement => {
-  console.log(props)
+// Components
+import Pagination from '../../components/pagination'
+import ListItem from '../../components/list-item'
 
-  return <div>Films page</div>
+interface Film {
+  characters: Array<string>
+  created: string
+  director: string
+  edited: string
+  episode_id: number
+  opening_crawl: string
+  planets: Array<string>
+  producer: string
+  release_date: string
+  species: Array<string>
+  starships: Array<string>
+  title: string
+  url: string
+  vehicles: Array<string>
 }
+
+interface FilmsProps {
+  page: number
+  total: number
+  next: string | null
+  previous: string | null
+  films: Array<Film>
+}
+
+const Films = ({ films, page, total, next, previous }: FilmsProps): ReactElement => (
+  <>
+    <div className=" flex flex-col justify-center text-center pt-16">
+      <ul className="grid grid-cols-5 justify-items-center gap-8 px-72">
+        {films.map((film, index) => {
+          const filmIndex = film.url.split('/').slice(5, -1)[0]
+
+          return (
+            <li key={index} className="w-full rounded-lg">
+              <ListItem
+                title={film.title}
+                link={filmIndex}
+                referece="films"
+                referenceId={filmIndex}
+                altMessage={film.title}
+              />
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+    <Pagination page={page} total={total} next={next} previous={previous} />
+  </>
+)
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const { data } = await api.get('films')
