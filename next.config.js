cc2c0888a178059ad80c8f62dda96e0d73300e9b
thales domingues <thdomingues.dev@gmail.com>
2022-04-25 02:01:03 -0300

    feat: remove missed file

--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["starwars-visualguide.com"]
-  }
+    domains: ['starwars-visualguide.com'],
+  },
+  i18n: {
+    locales: ['pt'],
+    defaultLocale: 'pt',
+  },
 }
 
 module.exports = nextConfig
